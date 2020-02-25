7b3862a46d93e45c05f511550ee6de0ae17ed857
MarjoleinAardewijn <marjolein.aardewijn@gmail.com>
2020-02-25 16:58:20 +0100

    localstorage and filters

--- docs/js/modules/data.js
+++ docs/js/modules/data.js
@@ -1,21 +1,43 @@
-// Todo: implement localStorage in app.
 export const data = {
-    get: function (key) {
+    /**
+     * Function to get the stored data back from localStorage.
+     *
+     * @param key -> the key which the data was stored with.
+     */
+    getItem: function (key) {
         return localStorage.getItem(key);
     },
 
-    set: function (key, data) {
+    /**
+     * Function to store the data in localStorage.
+     *
+     * @param key -> name of stored data
+     * @param data -> converted data array to string.
+     */
+    setItem: function (key, data) {
         localStorage.setItem(key, JSON.stringify(data));
     },
 
+    /**
+     * Function to convert the contents of localStorage back into something we can work with.
+     */
     parse: function (object) {
         return JSON.parse(object);
     },
 
+    /**
+     * Function to clear localStorage.
+     */
+    clear: function () {
+        localStorage.clear();
+    },
+
+    /**
+     * Function to filter the data and push to an array.
+     */
     filter: function (data) {
         let filter = [];
 
-        // filter the data and push to array.
         filter.push({
             id: data.objectNumber,
             title: data.title,
